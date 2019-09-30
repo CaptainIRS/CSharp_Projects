a9feb98ba3a089ce10fa87ec1220ebf0aa5fab80
CaptainIRS <36656347+CaptainIRS@users.noreply.github.com>
2019-09-30 15:34:57 +0530

    Update

--- BinaryTreeTest/Program.cs
+++ BinaryTreeTest/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using BinaryTree;
 
 namespace BinaryTreeTest
 {
@@ -10,6 +7,23 @@ namespace BinaryTreeTest
     {
         static void Main(string[] args)
         {
+            Tree<int> tree = new Tree<int>(10);
+            tree.Insert(5);
+            tree.Insert(11);
+            tree.Insert(5);
+            tree.Insert(-12);
+            tree.Insert(15);
+            tree.Insert(0);
+            tree.Insert(14);
+            tree.Insert(-8);
+            tree.Insert(10);
+            tree.Insert(8);
+            tree.Insert(8);
+
+            string sortedData = tree.WalkTree();
+            Console.WriteLine($"Sorted data is {sortedData}");
+
+            Console.ReadLine();
         }
     }
 }
