92043b1d0557557815d8c82dd6e6f4353d7a0b0f
CaptainIRS <36656347+CaptainIRS@users.noreply.github.com>
2019-09-30 15:18:36 +0530

    Binary Tree Test

---
+++ BinaryTree/Tree.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    public class Tree<TItem> where TItem : IComparable<TItem>
+    {
+        public TItem NodeData { get; set; }
+        public Tree<TItem> LeftTree { get; set; }
+        public Tree<TItem> RightTree { get; set; }
+
+        public Tree(TItem nodeValue)
+        {
+            this.NodeData = nodeValue;
+            this.LeftTree = null;
+            this.RightTree = null;
+        }
+
+        public void Insert(TItem newItem)
+        {
+            if (this.NodeData.CompareTo(newItem) > 0)
+            {
+                if (this.LeftTree == null)
+                {
+                    this.LeftTree = new Tree<TItem>(newItem);
+                }
+                else
+                {
+                    this.LeftTree.Insert(newItem);
+                }
+            }
+            else
+            {
+                if (this.RightTree == null)
+                {
+                    this.RightTree = new Tree<TItem>(newItem);
+                }
+                else
+                {
+                    this.RightTree.Insert(newItem);
+                }
+            }
+        }
+        public string WalkTree()
+        {
+            string result = "";
+
+            if (this.LeftTree != null)
+            {
+                result = this.LeftTree.WalkTree();
+            }
+
+            result += $" {this.NodeData.ToString()}";
+
+            if (this.RightTree != null)
+            {
+                result += this.RightTree.WalkTree();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ BinaryTreeTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BinaryTreeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
