8a8d2494b74ba81b0898216adb98c94b79c184f2
CaptainIRS <36656347+CaptainIRS@users.noreply.github.com>
2019-10-01 10:53:20 +0530

    VarianceTest added

---
+++ VarianceTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarianceTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Wrapper<string> stringWrapper = new Wrapper<string>();
+            IWrapper<string> storedStringWrapper = stringWrapper;
+            Console.WriteLine($"The stored data is : {storedStringWrapper.GetData()}");
+            //GetData() can be accessed only through interface as it is defined explicitly in the class
+            Console.ReadLine();
+        }
+    }
+}
---
+++ VarianceTest/Wrapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarianceTest
+{
+    class Wrapper<T> : IWrapper<T>
+    {
+        private T storedData;
+
+        T IWrapper<T>.GetData()
+        {
+            return this.storedData;
+        }
+
+        void IWrapper<T>.SetData(T data)
+        {
+            this.storedData = data;
+        }
+    }
+}
